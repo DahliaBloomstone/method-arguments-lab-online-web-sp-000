82a9bc05e923e2eb03848898b6f1e6d4c7185499
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-18 19:22:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,4 +6,5 @@ def introduction_with_language(name, language)
 puts "Hi, my name is #{name}, and I speak #{language}."
 name = "Dan"
 language = "Ember.js"
+puts "Hi, my name is #{name}" and I am learning to program in #{language}.""
 end
